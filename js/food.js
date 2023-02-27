07dcc61f4025ed949fa9802f7ea3f5c369eba6b9
Silvani-C <silvanicaitano@hotmail.com>
2023-02-27 10:05:17 -0300

    Silvani Food - HTML CSS e JS

---
+++ js/food.js
@@ -0,0 +1,6 @@
+function toggkeMenu() {
+    const menuToggle = document.querySelector('.menuTobble')
+    const navigation = document.querySelector('.navigation')
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
